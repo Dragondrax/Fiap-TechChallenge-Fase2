09b20414b427cf15ed1d62b34687b63069b0c490
Igor Hebling Sallowicz <Igor.Sallo@hotmail.com>
2024-05-16 23:08:00 -0300

    Autenticacao de Usuario

--- src/Services/Fiap.TechChallenge.Fase1.Dominio/Usuario/UsuarioService.cs
+++ src/Services/Fiap.TechChallenge.Fase1.Dominio/Usuario/UsuarioService.cs
@@ -1,8 +1,10 @@
 ﻿using Fiap.TechChallenge.Fase1.Data.Entidades;
 using Fiap.TechChallenge.Fase1.Data.Repository;
 using Fiap.TechChallenge.Fase1.Infraestructure.DTO;
+using Fiap.TechChallenge.Fase1.Infraestructure.DTO.Usuario;
 using Fiap.TechChallenge.Fase1.SharedKernel;
 using Fiap.TechChallenge.Fase1.SharedKernel.Model;
+using FluentValidation;
 using static BCrypt.Net.BCrypt;
 
 namespace Fiap.TechChallenge.Fase1.Dominio
@@ -43,6 +45,32 @@ namespace Fiap.TechChallenge.Fase1.Dominio
 
             _mensagem.Add(MensagemErroUsuario.MENSAGEM_USUARIO_JA_EXISTENTE);
 
+            return new ResponseModel(_mensagem, false, null);
+        }
+        public async Task<ResponseModel> AutenticarUsuario(AutenticarUsuarioDTO usuarioDto)
+        {
+            var validacao = new AutenticarUsuarioDTOValidator().Validate(usuarioDto);
+            if (!validacao.IsValid)
+            {
+                _mensagem = validacao.Errors.Select(x => x.ErrorMessage).ToList();
+                return new ResponseModel(_mensagem, false, null);
+            }
+
+            var usuario = await _usuarioRepository.ObterPorEmailAsync(usuarioDto.Email);
+
+            if(usuario is not null)
+            {
+                //Valida Senha
+                if(Verify(usuarioDto.Senha, usuario.Senha))
+                {
+                    var token = ""; //gerar token com os dados
+
+                    _mensagem.Add(MensagemErroGenerico.MENSAGEM_SUCESSO);
+                    return new ResponseModel(_mensagem, true, token);
+                }
+            }
+
+            _mensagem.Add(MensagemErroUsuario.MENSAGEM_USUARIO_LOGIN_SENHA_INCORRETA);
             return new ResponseModel(_mensagem, false, null);
         }
         private async Task<string> GerarHashSenhaUsuario(string senha)
---
+++ src/Services/Fiap.TechChallenge.Fase1.Infraestructure/DTO/Usuario/AutenticarUsuarioDTO.cs
@@ -0,0 +1,31 @@
+﻿using Fiap.TechChallenge.Fase1.SharedKernel;
+using FluentValidation;
+
+namespace Fiap.TechChallenge.Fase1.Infraestructure.DTO.Usuario
+{
+    public class AutenticarUsuarioDTO
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+
+    public class AutenticarUsuarioDTOValidator : AbstractValidator<AutenticarUsuarioDTO>
+    {
+        public AutenticarUsuarioDTOValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage(MensagemErroUsuario.MENSAGEM_EMAIL_NAO_PODE_SER_VAZIO)
+                .NotNull()
+                .WithMessage(MensagemErroUsuario.MENSAGEM_EMAIL_NAO_PODE_SER_NULO)
+                .EmailAddress()
+                .WithMessage(MensagemErroUsuario.MENSAGEM_EMAIL_NAO_ESTA_NO_FORMATO_CORRETO);
+
+            RuleFor(x => x.Senha)
+                .NotEmpty()
+                .WithMessage(MensagemErroUsuario.MENSAGEM_SENHA_NAO_PODE_SER_VAZIO)
+                .NotNull()
+                .WithMessage(MensagemErroUsuario.MENSAGEM_SENHA_NAO_PODE_SER_NULO);
+        }
+    }
+}
