9b2ce6b25c17e9c2dd7d63c8de9a3b1ebcfd4012
Igor Hebling Sallowicz <Igor.Sallo@hotmail.com>
2024-05-23 21:53:26 -0300

    Ajustando código para camada de dominio

--- src/Services/Fiap.TechChallenge.Fase1.IoC/InjecaoDependenciaConfig.cs
+++ src/Services/Fiap.TechChallenge.Fase1.IoC/InjecaoDependenciaConfig.cs
@@ -2,6 +2,7 @@
 using Fiap.TechChallenge.Fase1.Data.Repository;
 using Fiap.TechChallenge.Fase1.Data.Repository.Usuario;
 using Fiap.TechChallenge.Fase1.Dominio;
+using Fiap.TechChallenge.Fase1.Dominio.Token;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Fiap.TechChallenge.Fase1.IoC
@@ -14,6 +15,7 @@ namespace Fiap.TechChallenge.Fase1.IoC
 
             services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             services.AddScoped<IUsuarioService, UsuarioService>();
+            services.AddScoped<ITokenService, TokenService>();
 
             return services;
         }
