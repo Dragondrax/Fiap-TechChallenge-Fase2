8e88ea5e46ceae8a4290ba93779a1113dc785caa
caioandre182 <caioserralvo182@gmail.com>
2024-05-24 00:15:09 -0300

    feat: criação do endpoint completo de cadastro de contato, seguindo o padrão da api

---
+++ src/Services/Fiap.TechChallenge.Fase1.Dominio/Contato/ContatoService.cs
@@ -0,0 +1,43 @@
+﻿using Fiap.TechChallenge.Fase1.Data.Repository;
+using Fiap.TechChallenge.Fase1.Infraestructure.DTO.Contato;
+using Fiap.TechChallenge.Fase1.SharedKernel.Model;
+using Fiap.TechChallenge.Fase1.Data.Entidades;
+using Fiap.TechChallenge.Fase1.SharedKernel;
+using Fiap.TechChallenge.Fase1.SharedKernel.MensagensErro;
+
+
+namespace Fiap.TechChallenge.Fase1.Dominio;
+
+public class ContatoService(IContatoRepository contatoRepository) : IContatoService
+{
+    private readonly IContatoRepository _contatoRepository = contatoRepository;
+    private List<string> _mensagem = [];
+
+    public async Task<ResponseModel> SalvarContato(CriarContatoDTO contatoDTO)
+    {
+        var validacao = new CriarContatoDTOValidator().Validate(contatoDTO);
+
+        if (!validacao.IsValid)
+        {
+            _mensagem = validacao.Errors.Select(x => x.ErrorMessage).ToList();
+            return new ResponseModel(_mensagem, false, null);
+        }
+
+        var contato = await _contatoRepository.ObterPorEmailAsync(contatoDTO.Email);
+
+        if (contato is null)
+        {
+            var novoContato = new Contato(contatoDTO.Nome, contatoDTO.DDD, contatoDTO.Telefone, contatoDTO.Email);
+
+            await _contatoRepository.AdicionarAsync(novoContato);
+
+            _mensagem.Add(MensagemErroGenerico.MENSAGEM_SUCESSO);
+
+            return new ResponseModel(_mensagem, true, novoContato);
+        }
+
+        _mensagem.Add(MensagemErroContato.MENSAGEM_CONTATO_JA_EXISTENTE);
+
+        return new ResponseModel(_mensagem, false, null);
+    }
+}
--- src/Services/Fiap.TechChallenge.Fase1.IoC/InjecaoDependenciaConfig.cs
+++ src/Services/Fiap.TechChallenge.Fase1.IoC/InjecaoDependenciaConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Fiap.TechChallenge.Fase1.Data.Context;
 using Fiap.TechChallenge.Fase1.Data.Repository;
+using Fiap.TechChallenge.Fase1.Data.Repository.Contato;
 using Fiap.TechChallenge.Fase1.Data.Repository.Usuario;
 using Fiap.TechChallenge.Fase1.Dominio;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,6 +15,8 @@ namespace Fiap.TechChallenge.Fase1.IoC
 
             services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             services.AddScoped<IUsuarioService, UsuarioService>();
+            services.AddScoped<IContatoRepository, ContatoRepository>();
+            services.AddScoped<IContatoService, ContatoService>();
 
             return services;
         }
