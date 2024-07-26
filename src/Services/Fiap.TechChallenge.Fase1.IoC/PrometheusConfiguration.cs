160d7e28b026d0ff9e7bad528755dd0ff522c77a
Igor Hebling Sallowicz <Igor.Sallo@hotmail.com>
2024-07-25 21:38:01 -0300

    dashboard grafana

--- src/Services/Fiap.TechChallenge.Fase1.IoC/PrometheusConfiguration.cs
+++ src/Services/Fiap.TechChallenge.Fase1.IoC/PrometheusConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Prometheus;
+using System.Diagnostics;
 
 namespace Fiap.TechChallenge.Fase1.IoC
 {
@@ -10,12 +11,32 @@ namespace Fiap.TechChallenge.Fase1.IoC
         {
             var counter = Metrics.CreateCounter("FiapMetrics", "Metricas API", new CounterConfiguration
             {
-                LabelNames = new[] { "method", "endpoint" }
+                LabelNames = new[] { "method", "endpoint" },
+
             });
 
             app.Use((context, next) =>
             {
                 counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+                return next();
+            });
+
+            app.Use((context, next) =>
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                context.Response.OnStarting(() =>
+                {
+                    stopwatch.Stop();
+                    var elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+
+                    var metric = Metrics.CreateHistogram("http_request_duration_milliseconds", "Duration of HTTP requests in milliseconds.");
+                    metric.Observe(elapsedMilliseconds);
+
+                    return Task.CompletedTask;
+                });
+
                 return next();
             });
 
